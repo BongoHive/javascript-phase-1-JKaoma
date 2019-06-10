48263dd6ab166375d576b3c8f60280a66136fd12
James Kaoma <jameskaoma84@outlook.com>
2019-06-10 14:55:45 -0800

    completed stage 1

--- variables.js
+++ variables.js
@@ -1,1 +1,15 @@
 // you solution should be here
+
+var birthYear = 1996;
+var futureYear = 2030;
+var ages = futureYear - birthYear;
+console.log(ages + 'or' + (ages - 5));
+
+var currentAge = 23;
+var maxAge = 30;
+var estAmount = 20;
+var estTotal = estAmount * 365 * (maxAge - currentAge);
+console.log(estTotal);
+
+var message = 'You will need ' + estTotal + ' supplies to last you until the ripe age of ' + maxAge;
+console.log(message);
